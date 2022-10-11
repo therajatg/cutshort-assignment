f97a90727ce9df2313ae77c4e4259a56fe302e4e
Rajat Gupta <dguptarajat@gmail.com>
2022-10-11 18:22:00 +0530

    style - progress bar

--- src/components/progressIndicator/ProgressIndicator.js
+++ src/components/progressIndicator/ProgressIndicator.js
@@ -3,29 +3,39 @@ import { useLocation, Outlet } from "react-router-dom";
 import style from "./progressIndicator.module.css";
 import { useState } from "react";
 
-const stepNumbers = [
-  { step: 1, path: "/" },
-  { step: 2, path: "/step2" },
-  { step: 3, path: "/step3" },
-  { step: 4, path: "/step4" },
-];
 export const ProgressIndicator = () => {
+  const stepNumbers = [1, 2, 3, 4];
   const { pathname } = useLocation();
   const [displayName, setDisplayName] = useState("");
-  console.log(displayName);
+  const progress = { undefined: 1, 2: 3, 3: 5, 4: 6 };
   return (
     <div className={style.window}>
       <div className={style.main}>
         <h2>
           <RiLeafFill /> Eden
         </h2>
-        <div className={style.allSteps}>
-          {stepNumbers.map(({ step, path }) => (
-            <div className={pathname === path ? style.step : ""} key={step}>
-              {step}
-            </div>
-          ))}
+        <div className={style.userProgress}>
+          <div className={style.allSteps}>
+            {stepNumbers.map((step) => (
+              <div
+                className={
+                  Number(pathname.split("")[5]) >= step || step === 1
+                    ? style.activeStep
+                    : style.step
+                }
+                key={step}
+              >
+                {step}
+              </div>
+            ))}
+          </div>
+          <progress
+            max="6"
+            value={progress[pathname.split("")[5]]}
+            className={style.progressBar}
+          ></progress>
         </div>
+
         <Outlet context={[displayName, setDisplayName]} />
       </div>
     </div>
